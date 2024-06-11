49df26d899498b10a5a28e8f4b9db2c5a652e5db
Nikhil239311 <nikhilsutar1910@gmail.com>
2024-06-11 15:03:14 +0530

    Add files via upload

---
+++ Function_name.java
@@ -0,0 +1,14 @@
+package day_2;
+
+public class Function_name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Function_test obj=new Function_test();
+		
+		Function_test.add();
+		Function_test.sub(25,15);
+		
+	}
+
+}
---
+++ Function_test.java
@@ -0,0 +1,16 @@
+package day_2;
+
+public class Function_test {
+
+	public static void add() {
+		int a=10;
+		int b=20;
+		int total=a+b;
+		System.out.println("Addition="+total);
+	}
+	public static void sub(int c,int d) {
+		
+		int total=c-d;
+		System.out.println("Sub="+total);
+	}
+}
